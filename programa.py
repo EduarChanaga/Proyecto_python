5345beff7d3b3baa8006ae713ed3e49111d30186
eduar <chanaga250@gmail.com>
2024-02-11 23:31:42 -0500

    correciones de tabulacion

--- programa.py
+++ programa.py
@@ -3,9 +3,15 @@
 # Proyecto campuslands "python" 9 de febrero *
 # ********************************************
 # ********************************************
-
 import json
 import uuid
+import os
+
+def clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 # Abrir el archivo JSON
 with open('datos.json', 'r') as datos:
     # Cargar el contenido del archivo en un diccionario
@@ -14,32 +20,36 @@ with open('datos.json', 'r') as datos:
 with open('aulas.json', 'r') as aulas:
     # Cargar el contenido del archivo en un diccionario
     aulass = json.load(aulas)
+print("\033[1;37m##############################")
+print("# Bienvenido a Campuslands   #")
+print("#  (Centro de formacion)     #")
+print("##############################")
+input("---> Enter para entrar <---")
 while True:
+    clear()
     inscritos = inscrip["inscripcion"]["Inscritos"]
     aprobados = inscrip["inscripcion"]["aprobados"]
     reprobados=inscrip["inscripcion"]["reprobados"]
-    print("##############################")
-    print("# Bienvenido a Campuslands   #")
-    print("#  (Centro de formacion)     #")
-    print("##############################")
-    print("----------------------------------------")
+
     print("Que tipo de usuario desea ingresar?")
     print("1. Camper\t2. Trainer\t3. Cordinador")
 
     Decision1=int(input("--> "))
-    print("--------------------")
+
 
 
     ######################### Campers #########################
     if Decision1==1:
         Decision2=0
+        clear()
         while Decision2!=3:
             print("Que desea realizar?")
             print("1. Inscripcion")
             print("2. Ver datos de campers")
             print("3. salir")
             Decision2=int(input("--> "))
-            print("--------------------")
+            clear()
+          
 
             
             if Decision2==1:
@@ -82,6 +92,7 @@ while True:
 
         ######################### Trainers #########################
     if Decision1==2:
+        clear()
         print("")
 
 
@@ -95,51 +106,60 @@ while True:
 
         ######################### Coordinador #########################
     if Decision1==3:
+        clear()
         decision2=0
+        print("Porfavor digite la contraseña")
+        contraseña=str(input("--> "))
         while decision2!=2:
-            print("Porfavor digite la contraseña")
-            contraseña=str(input("--> "))  #campus2024
+              #campus2024
+            clear()
             if contraseña=="campus2024":
-                print("-----")
+               
                 print("Que desea realizar?")
                 print("1. Ingresar notas de estudiantes inscritos")
                 print("2. salir")
                 decision2=int(input("--> "))
-                print("-----")
-
-
+                
+                clear()
+                
                             #definir si un estudiante inscrito aprueba o reprueba
 
                 if decision2==1: 
-                    inscritos_ordenados = sorted(inscritos, key=lambda x: x["Id"])
-                    for inscrito in inscritos_ordenados:
-                        print("  N° Identificación:", inscrito["n_identificacion"], "  Nombre:", inscrito["Nombre"] )
-                    print("-----")
-                    identificacion_nota_inscrito=int(input("N° de identificacion del estudiante a ingresar notas: "))
-                    nota_teorica_inscrito=float(input("Nota teorica: "))
-                    nota_practica_inscrito=float(input("Nota practica: "))
-                    calificacion_inscrito=(nota_practica_inscrito+nota_teorica_inscrito)/2
-                    for inscrito in inscritos:
-                        if inscrito["n-identificacion"] == identificacion_nota_inscrito:
-                            # Actualizar el estado del inscrito si la calificación es mayor o igual a 60
-                            if calificacion_inscrito >= 60:
-                                inscrito["Estado"] = "Aprobado"
-                                    # Mover al estudiante de la lista de inscritos a la lista de aprobados
-                                    # Generar una nueva ID única para el estudiante aprobado
-                                    
-                                aprobados.append(inscrito)
-                                inscritos.remove(inscrito)
-                            else:
-                                inscrito["Estado"] = "Reprobado"
-                                reprobados.append(inscrito)
-                                inscritos.remove(inscrito)
-                            break
-
-                        # Escribir el JSON actualizado de vuelta al archivo
-                    with open('datos.json', 'w') as file:
-                        json.dump(inscrip, file, indent=2)
+                    f="si"
+                    while f=="si":
+                        inscritos_ordenados = sorted(inscritos, key=lambda x: x["Id"])
+                        for inscrito in inscritos_ordenados:
+                            print("  N° Identificación:", inscrito["n_identificacion"], "  Nombre:", inscrito["Nombre"] )
+                        print("-----")
+                        identificacion_nota_inscrito=int(input("N° de identificacion del estudiante a ingresar notas: "))
+                        nota_teorica_inscrito=float(input("Nota teorica: "))
+                        nota_practica_inscrito=float(input("Nota practica: "))
+                        calificacion_inscrito=(nota_practica_inscrito+nota_teorica_inscrito)/2
+                        for inscrito in inscritos:
+                            if inscrito["n_identificacion"] == identificacion_nota_inscrito:
+                                # Actualizar el estado del inscrito si la calificación es mayor o igual a 60
+                                if calificacion_inscrito >= 60:
+                                    inscrito["Estado"] = "Aprobado"
+                                        # Mover al estudiante de la lista de inscritos a la lista de aprobados
+                                        # Generar una nueva ID única para el estudiante aprobado
+                                        
+                                    aprobados.append(inscrito)
+                                    inscritos.remove(inscrito)
+                                else:
+                                    inscrito["Estado"] = "Reprobado"
+                                    reprobados.append(inscrito)
+                                    inscritos.remove(inscrito)
+                                
+                                
+                        
+                                with open('datos.json', 'w') as file:
+                                    json.dump(inscrip, file, indent=2)
+                                print("Desea ingresar notas de un estudiante? (si / no)")
+                                f=str(input("--> "))
+                                break
             else:
                 print("contraseña incorrecta")
+                decision2=2
 
 ## Desarrollado por: 
 ##Eduar Damian Chanaga Gonzalez - 1095581647
