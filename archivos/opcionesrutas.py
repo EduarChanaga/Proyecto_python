513e47f17590003615f0b7c2255ea45a19d628d9
eduar <chanaga250@gmail.com>
2024-02-13 14:06:34 -0500

    grupos

--- archivos/opcionesrutas.py
+++ archivos/opcionesrutas.py
@@ -9,13 +9,15 @@ def crear_grupo():
         else:
             os.system('clear')
     while True:
-        with open('info_grupos.json', 'r') as proceso_file:
-            info_grupos = json.load(proceso_file) 
-        with open('grupos.json', 'r') as proceso_file:
-            grupos = json.load(proceso_file)
-        with open('trainers.json', 'r') as proceso_file:
-            trainers = json.load(proceso_file) # Cargar contenido del archivo JSON a una variable
+        with open('info_grupos.json', 'r') as i:
+            info_grupos = json.load(i) 
+        with open('grupos.json', 'r') as o:
+            grupos = json.load(o)
+        with open('trainers.json', 'r') as u:
+            trainers = json.load(u) # Cargar contenido del archivo JSON a una variable
         clear()
+        
+        
         print(" - Proceso de creacion de grupos - ")
         print("")
         print("Desea crear nuevo grupo? (Si / No)")
@@ -34,39 +36,65 @@ def crear_grupo():
             print("ID:", trainer["n_identificacion"])
             print("Nombre:", trainer["Nombre"])
           
+          
+          
+          
+        #Agregar entrenador a nuevo grupo
         while True:
-            selected_id = input("Ingresa la ID del entrenador que deseas seleccionar: ")
+            grupo = input("Ingresa la ID del entrenador que deseas seleccionar: ")
 
             # Buscar el entrenador por su ID
-            selected_trainer = None
+            grupo_trainer = None
             for trainer in trainers["campers"]["trainers"]:
-                if str(trainer["n_identificacion"]) == selected_id:
-                    selected_trainer = {
-                        "ID": selected_id,
+                if str(trainer["n_identificacion"]) == grupo:
+                    grupo_trainer = {
+                        "ID": grupo,
                         "Nombre": trainer["Nombre"]
                     }
                     break
 
-            if selected_trainer:
-                print("Entrenador seleccionado:", selected_trainer["Nombre"])
+            if grupo_trainer:
+                print("Entrenador seleccionado:", grupo_trainer["Nombre"])
                 break
             else:
                 print("La ID del entrenador ingresada no existe. Intenta de nuevo.")
+                
+                
+        #definir aula del salon
+        print("1. Apolo")
+        print("2. Artemis")
+        print("3. Sputnik")
+        grupo_salon=int(input("Ingrese N° de salon: "))
         
+        
+        #definir ruta del salon
+        print("1. Ruta NodeJS")
+        print("2. Ruta Java")
+        print("3. Ruta NetCore")
+        grupo_ruta=int(input("Ingrese N° de ruta: "))
+        
+        
+        
+        #Guardar datos en un diccionario
         nuevo_dato = {
             "nombre_grupo": grupo_nombre,
-            "Horario 1": grupo_horario,
-            "trainer": selected_trainer,
-            "ruta": ""
+            "Horario": grupo_horario,
+            "trainer": grupo_trainer,
+            "Salon":grupo_salon,
+            "Ruta":grupo_ruta
         }
         
         # Agregar el grupo al archivo JSON
-        # ...
 
-        print("Ingreso del grupo exitoso!")
-
+    
+ 
         #Agregar trainer al json ( trianers.json)
         info_grupos["campus"]["grupo"].append(nuevo_dato) 
+        print("Ingreso del grupo exitoso!")
+        nuevo_grupo = grupo_nombre
+        grupos["grupos"][nuevo_grupo] = []
+        with open('grupos.json', 'w') as file:
+            json.dump(grupos, file, indent=2) 
 
         with open('info_grupos.json', 'w') as file:
             json.dump(info_grupos, file, indent=2)  # Guardar cambios
