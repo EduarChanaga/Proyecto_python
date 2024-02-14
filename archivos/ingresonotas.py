95a31c17b95b29f2c43e1d1484f7b7fa2da44e23
eduar <chanaga250@gmail.com>
2024-02-14 07:57:17 -0500

    Ingreso de notas

--- archivos/ingresonotas.py
+++ archivos/ingresonotas.py
@@ -1,8 +1,41 @@
 import json
-with open("grupos.json", "r") as grups:
-    grupos = json.load(grups)
-with open("trainers.json", "r") as file:
-    trainers = json.load(file)
-        
+
 def ingreso_notas():
-    print("Bienvenido trainer ")
+    with open("grupos.json", "r") as file:
+        grupos = json.load(file)
+
+    print("Grupos disponibles:")
+    for grupo in grupos["grupos"]:
+        print(grupo)
+
+    grupo_seleccionado = input("Ingrese el nombre del grupo que desea seleccionar: ")
+
+    if grupo_seleccionado in grupos["grupos"]:
+        alumnos = grupos["grupos"][grupo_seleccionado]
+        print("\nAlumnos en el grupo:")
+        for alumno in alumnos:
+            print(f"ID: {alumno['n_identificacion']} - Nombre: {alumno['Nombre']}")
+
+        id_alumno = int(input("\nIngrese el ID del alumno para ingresar la nota: "))
+        for alumno in alumnos:
+            if alumno['n_identificacion'] == id_alumno:
+                print("\nModulos del alumno:")
+                for i, modulo in enumerate(alumno["modulos"], 1):
+                    print(f"{i}. {modulo}")
+
+                modulo_seleccionado = int(input("\nIngrese el número del modulo al que desea ingresar la nota: "))
+                if 1 <= modulo_seleccionado <= len(alumno["modulos"]):
+                    modulo = list(alumno["modulos"].keys())[modulo_seleccionado - 1]
+                    nota = float(input(f"Ingrese la nota para el modulo '{modulo}': "))
+                    alumno["modulos"][modulo] = nota
+                    print("Nota ingresada correctamente.")
+                else:
+                    print("El número de módulo ingresado no es válido.")
+                break
+        else:
+            print("Alumno no encontrado en el grupo.")
+    else:
+        print("El grupo ingresado no existe.")
+
+    with open("grupos.json", "w") as file:
+        json.dump(grupos, file, indent=4)
